320230de3af4279cfa6eaae2d49af21a5d51293e
ameyabap@gmail.com <ameyabap@gmail.com>
2019-09-13 19:42:41 +0530

    error handling

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Bitcoin ...
 type Bitcoin int
@@ -19,4 +22,20 @@ func (w *Wallet) Deposit(amt Bitcoin) {
 // Balance ...
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+// Withdraw ...
+func (w *Wallet) Withdraw(amt Bitcoin) error {
+	if w.balance < amt {
+		return ErrInsufficientBalance
+	}
+	w.balance -= amt
+	return nil
+}
+
+// ErrInsufficientBalance ...
+var ErrInsufficientBalance = errors.New("Can't Withdraw, Insufficient Balance")
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,7 +1,6 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,13 +8,39 @@ import (
 
 //
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		assert.Equal(t, want, wallet.Balance())
+	}
+	assertError := func(t *testing.T, want error, got error) {
+		//assert.Equal(t, want, got)
+		assert.Equal(t, want, got)
+	}
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-	assert.Equal(t, want, got)
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{
+			balance: Bitcoin(20),
+		}
+
+		err := wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+		assertError(t, err, nil)
+	})
+
+	t.Run("insufficient balance", func(t *testing.T) {
+		wallet := Wallet{
+			balance: Bitcoin(20),
+		}
+
+		err := wallet.Withdraw(Bitcoin(30))
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientBalance)
+	})
+
 }
